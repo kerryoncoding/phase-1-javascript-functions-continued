311d4232767fd8e848bb40b0e1e9ab1c526c890e
Kerry Guarino <kerry.guarino@gmail.com>
2023-04-27 20:33:27 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+function saturdayFun(funThing) {
+   if (funThing){
+      return (`This Saturday, I want to ${funThing}!` )
+   } else {
+      return (`This Saturday, I want to roller-skate!`)
+   }
+}
+
+function mondayWork(work) {
+   if (work){
+      return (`This Monday, I will ${work}.`)
+   } else {
+      return (`This Monday, I will go to the office.`)
+   }
+}
+
+function wrapAdjective(flair) {
+   let specialWord = "special"
+   return function (adjective) {
+      if (adjective) {
+         return (`You are ${flair}${adjective}${flair}!`) 
+      } else {
+         return (`You are ${flair}${specialWord}${flair}!`)
+      }
+   }
+}
+
+
+
